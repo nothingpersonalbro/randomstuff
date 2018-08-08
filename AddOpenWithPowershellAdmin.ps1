aba1bcf6111955d0010ac0a9c554aa5791ab78a3
nothingpersonalbro <40741328+nothingpersonalbro@users.noreply.github.com>
2018-08-08 10:55:27 -0400

    Add files via upload

---
+++ AddOpenWithPowershellAdmin.ps1
@@ -0,0 +1,13 @@
+# This will add 'open powershell here as administrator' option for shift right click in a folder context menu.
+# This must be ran as administrator
+
+$Menu = 'Open PowerShell window here (Admin)'
+$Command = "$PSHOME\powershell.exe -NoExit -NoProfile -Command ""Set-Location '%V'"""
+
+'directory', 'directory\background', 'drive' | ForEach-Object {
+    New-Item -Path "Registry::HKEY_CLASSES_ROOT\$_\shell" -Name runas\command -Force |
+    Set-ItemProperty -Name '(default)' -Value $Command -PassThru |
+    Set-ItemProperty -Path {$_.PSParentPath} -Name '(default)' -Value $Menu -PassThru |
+    Set-ItemProperty -Name HasLUAShield -Value '' -PassThru |
+    Set-ItemProperty -Name Extended -Value ''
+}
